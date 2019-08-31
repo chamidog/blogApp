f814e3b4e1fae495bd1383c87daab7510f67220e
shinji.misaka <shinmisa0114@gmail.com>
2019-09-01 06:50:22 +0900

    ブログアプリを製作した。

--- blogApp/ViewController.swift
+++ blogApp/ViewController.swift
@@ -1,20 +1,32 @@
-//
-//  ViewController.swift
-//  blogApp
-//
-//  Created by 三坂真治 on 2019/09/01.
-//  Copyright © 2019 shinji.misaka. All rights reserved.
-//
-
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKUIDelegate{
+    
+    private var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // WKWebViewを生成
+        webView = WKWebView(frame:CGRect(x:0, y:0, width:self.view.bounds.size.width, height:self.view.bounds.size.height))
+        
+        // URL設定
+        let urlString = "https://chamidog.com"
+        let encodedUrlString = urlString.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)
+        
+        let url = NSURL(string: encodedUrlString!)
+        let request = NSURLRequest(url: url! as URL)
+        
+        webView.load(request as URLRequest)
+        
+        self.view.addSubview(webView)
+        
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 }
-
